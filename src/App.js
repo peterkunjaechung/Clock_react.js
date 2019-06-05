40e8e21b42fa5bc1ac10c5b668a5c9bbbed80232
contactpeterchung.com <contactpeterchung@gmail.com>
2019-06-05 11:32:19 -0600

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Clock from "./Clock";
+import { Button, Container, } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { showClock: false, };
+
+  toggleShowClock = () => {
+    this.setState( state => {
+      return { showClock: !state.showClock, };
+    })
+  }
+
+  render() {
+    return (
+    <Container style={{ marginTop: "25px", }}>
+      { this.state.showClock && <Clock /> }
+      <Button onClick={this.toggleShowClock} color="purple">Toggle Clock</Button>
+      </Container>
+    );
+  }
 }
 
 export default App;
